3f2ebd2a56570f8bf9278de405f95208261d93b3
bmatchey <bmatchey@frontiernet.net>
2015-03-29 23:45:43 -0500

    Fixed up full pic modal dialog.

--- FurnitureOutlet/WebContent/app/components/pictures/picModalController.js
+++ FurnitureOutlet/WebContent/app/components/pictures/picModalController.js
@@ -1,6 +1,47 @@
 angular.module('app.picModal', [])
-	.controller('picModalCtrl', PicModalCtrl);
+	.controller('picModalCtrl', PicModalCtrl)
+	.directive('modalDialog', modalDialog);
 
-function PicModalCtrl()
+function PicModalCtrl(logger)
 {
+	var vm = this;
+	vm.modalShown = false;
+	vm.toggleModal = toggleModal;
+	
+	function toggleModal()
+	{
+		vm.modalShown = !vm.modalShown;
+		logger.info('toggleModal clicked - shown = ' + vm.modalShown);
+	}
+}
+
+function modalDialog()
+{
+	var directive = {
+	    restrict: 'E',
+	    scope: {
+	      show: '='
+	    },
+	    replace: true, // Replace with the template below
+	    transclude: true, // we want to insert custom content inside the directive
+        controller: PicModalCtrl,
+        controllerAs: 'picCtrl',
+        link: linkFunc,
+        template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='picCtrl.hideModal()'></div><div class='ng-modal-dialog' ng-style='picCtrl.dialogStyle'><div class='ng-modal-close' ng-click='picCtrl.hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+	    };
+	
+	return directive;
+	  
+	function linkFunc(scope, element, attrs, picCtrl)
+	{
+		picCtrl.dialogStyle = {};
+	      if (attrs.width)
+	    	  picCtrl.dialogStyle.width = attrs.width;
+	      if (attrs.height)
+	    	  picCtrl.dialogStyle.height = attrs.height;
+	      picCtrl.hideModal = function() 
+	      {
+	    	  scope.show = false;
+	      };
+	}
 }
